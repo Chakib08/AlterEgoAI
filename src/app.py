3b05b6972d59626a2bb245180643dbfc87bc4ac2
Mohamed Lamine KARTOBI <chakibdace@gmail.com>
2025-07-20 13:52:49 +0200

    Listen to host 0.0.0.0 for deployment

--- src/app.py
+++ src/app.py
@@ -77,7 +77,12 @@ def generate():
             f.close()
 
 def main() -> None:
-    app.run()
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
 if __name__ == "__main__":  
     main()
